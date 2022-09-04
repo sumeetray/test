ddc64f1384472d7b8ed49dd97eba853807a47928
Sumeet Ray <sumeetray44@gmail.com>
2022-09-04 22:51:39 +0545

    resolved

--- scrap.py
+++ scrap.py
@@ -13,28 +13,25 @@ def scrap():
     try: 
         with open('annapurna_post.json', encoding='utf-8') as f:
             json_data = json.load(f)
-            title = json_data.get('metadata').get('title')
-            page = json_data.get('metadata').get('page') + 1
+            title = json_data.get('data').get('title')
+            page = json_data.get('data').get('page') + 1
     except:
-        json_data["metadata"]=params
-        json_data["data"]=[]
+        json_data["data"]=params
+        json_data["data"]["articles"]=[]
         json.dump(json_data, open('annapurna_post.json', 'w'), indent=4)
-        title = json_data.get('metadata').get('title')
-        page = json_data.get('metadata').get('page') + 1
+        title = json_data.get('data').get('title')
+        page = json_data.get('data').get('page') + 1
     temp_page = page
     while(page<=temp_page+2):
         try:
             params['page']=page
             params['title']=title
-            json_each_page = {}
             res = requests.get(url, params=params)
             data = res.json()
             if (res.status_code == 200 ):
-                json_data["metadata"]['page'] = params['page']
-                json_each_page["current_page"] = params["page"]
-                json_each_page["article"] = data["data"]["items"]
-                json_each_page["title"] = params["title"]
-                json_data["data"].append(json_each_page)
+                json_data["data"]['page'] = params['page']
+                for i in data["data"]["items"]:
+                    json_data["data"]["articles"].append(i)
                 json.dump(json_data, open('annapurna_post.json', 'w'), indent=4)
                 print("Page:", page, 'success')
                 page = page + 1
